763a66829e85530026a3986fbf87e422d9cc441a
AdrianDonate <adrianmc1500@gmail.com>
2021-12-22 19:00:53 +0100

    Subida de ficheros con cobertura de código realizada

--- Problema2/src/test/java/B03/Problema2/AppTest.java
+++ Problema2/src/test/java/B03/Problema2/AppTest.java
@@ -16,5 +16,36 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+        //calcularPrecio(int iaclm, Persona p, int plazas) 	
+        //Persona(boolean enferma, boolean sintomas, boolean contacto, boolean pasaporte, boolean profimpre, int edad)
+        
+       Persona p1 = new Persona(true, true, true, false, false, 20);
+       Persona p2 = new Persona(false, false, false, true, false, 18);
+       Persona p3 = new Persona(false, false, false, true, true, 18); // profimpre true
+       Persona p4 = new Persona(false, false, false, true, false, 70);
+       Persona p5 = new Persona(false, false, false, true, true, 70);  //prof impre true
+       
+       Principal.calcularPrecio(0, p1, 80);
+       Principal.calcularPrecio(0, p2, 80);
+       Principal.calcularPrecio(50, p2, 80);
+       Principal.calcularPrecio(50, p4, 80);
+       Principal.calcularPrecio(150, p2, 80);
+       Principal.calcularPrecio(150, p4, 80);
+       Principal.calcularPrecio(250, p3, 80);
+       Principal.calcularPrecio(250, p5, 80);
+       Principal.calcularPrecio(250, p2, 80);
+       Principal.calcularPrecio(250, p4, 80);
+       Principal.calcularPrecio(400, p3, 80);
+       Principal.calcularPrecio(400, p5, 80);
+       Principal.calcularPrecio(400, p2, 80);
+       Principal.calcularPrecio(400, p4, 80);
+       Principal.calcularPrecio(600, p3, 80);
+       Principal.calcularPrecio(600, p5, 80);
+       Principal.calcularPrecio(600, p2, 80);
+       Principal.calcularPrecio(600, p4, 80);
+       
+       
+       
+       
     }
 }
